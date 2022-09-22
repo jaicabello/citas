d12e0ffdd484ebe9a934309c690b8966ce52e8f4
Jaime <jai.cabellov@gmail.com>
2022-09-21 22:59:41 -0300

    Se agregan estilos al componente paciente

--- src/components/Paciente.js
+++ src/components/Paciente.js
@@ -3,12 +3,49 @@ import {Text, View, StyleSheet} from 'react-native';
 
 export const Paciente = ({item}) => {
   const {paciente, fecha} = item;
+
+  const formatearFecha = fecha => {
+    const nuevaFecha = new Date(fecha);
+    const opciones = {
+      weekday: 'long',
+      year: 'numeric',
+      month: 'long',
+      day: 'numeric',
+    };
+    return nuevaFecha.toLocaleDateString('es-ES', opciones);
+  };
+
   return (
-    <View>
-      <Text>{paciente}</Text>
-      <Text>{fecha}</Text>
+    <View style={styles.contenedor}>
+      <Text style={styles.label}>Paciente</Text>
+      <Text style={styles.texto}>{paciente}</Text>
+      <Text style={styles.fecha}>{formatearFecha(fecha)}</Text>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  contenedor: {
+    backgroundColor: '#FFF',
+    padding: 20,
+    borderBottomColor: '#94a3B8',
+    borderBottomWidth: 1,
+  },
+  label: {
+    color: '#374151',
+    textTransform: 'uppercase',
+    fontWeight: '700',
+    marginBottom: 10,
+  },
+  texto: {
+    color: '#6D28D9',
+    fontSize: 24,
+    fontWeight: '700',
+    marginBottom: 10,
+  },
+  fecha: {
+    color: '#374151',
+  },
+});
 
 export default Paciente;
