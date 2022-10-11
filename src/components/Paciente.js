9329613a921abb022aca30d77e50b1250ab8694d
Jaime Cabello <jai.cabellov@gmail.com>
2022-10-11 18:30:56 -0300

    Se avanza en la clase del curso

--- src/components/Paciente.js
+++ src/components/Paciente.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Text, View, StyleSheet} from 'react-native';
+import {Text, View, StyleSheet, Pressable} from 'react-native';
 
 export const Paciente = ({item}) => {
   const {paciente, fecha} = item;
@@ -20,6 +20,14 @@ export const Paciente = ({item}) => {
       <Text style={styles.label}>Paciente</Text>
       <Text style={styles.texto}>{paciente}</Text>
       <Text style={styles.fecha}>{formatearFecha(fecha)}</Text>
+      <View style={styles.contenedorBotones}>
+        <Pressable style={[styles.btn, styles.btnEditar]}>
+          <Text style={styles.btnTexto}>Editar</Text>
+        </Pressable>
+        <Pressable style={[styles.btn, styles.btnEliminar]}>
+          <Text style={styles.btnTexto}>Eliminar</Text>
+        </Pressable>
+      </View>
     </View>
   );
 };
@@ -46,6 +54,19 @@ const styles = StyleSheet.create({
   fecha: {
     color: '#374151',
   },
+  contenedorBotones: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginTop: 20,
+  },
+  btn: {},
+  btnEditar: {
+    backgroundColor: '#F59E0B',
+  },
+  btnEliminar: {
+    backgroundColor: '#EF4444',
+  },
+  btnTexto: {},
 });
 
 export default Paciente;
